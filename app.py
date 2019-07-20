162acbcf0b684c861c065e0860a1e8bb2275660c
airpollutiondata <53104105+airpollutiondata@users.noreply.github.com>
2019-07-19 23:06:58 -0400

    Added pandas lib

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import pandas as pd
+
 
 ########### Set up the chart
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
