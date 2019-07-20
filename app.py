2c3089e20034ddc0d0a9916483f3b6b687b31ea5
airpollutiondata <53104105+airpollutiondata@users.noreply.github.com>
2019-07-20 17:02:43 -0400

    Moving over more global info

--- app.py
+++ app.py
@@ -7,11 +7,33 @@ import numpy as np
 from dash.dependencies import Input, Output
 
 # Testing
+
+# Allows for the map generation
+mapbox_access_token = 'pk.eyJ1IjoiamhhbW1lcmVkMjQiLCJhIjoiY2p4eHZ2ODh0MDN1cjNtcnRrbjhkemliayJ9.kF1WMOd0RepqBUqdfXaJFw'
+
+# Metric used for plotting
+plottingParameter = '95th Percentile'
+
+# Read in the measurements from the binary file
 data = pd.read_hdf('air_pollution_data.h5', 'df')
 
 # Table containing the limits for the EPA
 limitFile = 'EPA Limit Table.xlsx'
 Limits = pd.read_excel(limitFile)
+
+
+# Calculate the total risk found by adding up the individual risks for each parameter
+# ------------------------------------------------
+siteDf_PerParameter = data[['Local Site Name', 'State Name','County Name', 'City Name', 'Latitude', 'Longitude','Year', 'Parameter Name', 'Risk Level']].groupby(['Local Site Name', 'State Name','County Name', 'City Name', 'Latitude', 'Longitude', 'Year', 'Parameter Name']).max().reset_index()
+siteDf_PerYear = siteDf_PerParameter[['Local Site Name', 'State Name','County Name', 'City Name', 'Latitude', 'Longitude', 'Year', 'Risk Level']].groupby(['Local Site Name', 'State Name','County Name', 'City Name', 'Latitude', 'Longitude', 'Year']).sum().reset_index()
+siteDf = siteDf_PerYear[['Local Site Name', 'State Name','County Name', 'City Name', 'Latitude', 'Longitude', 'Risk Level']].groupby(['Local Site Name', 'Latitude', 'Longitude']).max().reset_index()
+# Build the string to be used for hoovering on the map
+siteDf['Location'] = siteDf['City Name'].str.split(pat='(', expand=True)[0] + ', ' + siteDf['State Name']
+siteDf['Risk Level'] = siteDf['Risk Level'].round(decimals = 2)
+siteDf['Plotting Text'] = 'Risk: ' + siteDf['Risk Level'].astype(str) + ' extra cancer cases per 100k people<br><br>' + siteDf['Local Site Name'] + '<br>' + siteDf['Location'] 
+# Sort so that the highest risks are plotted last and they will end up on top
+siteDf = siteDf.sort_values(by=['Risk Level'])
+
 
 
 ########### Set up the chart
